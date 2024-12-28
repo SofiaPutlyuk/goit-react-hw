e3251196d066de10694f3485a390fbc6fe4a4f31
Sofia <putlyuksophia@gmail.com>
2024-12-28 20:28:18 +0200

    hw3

---
+++ my-app/src/friends.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../src/index.css";
+export const FriendList = ({friends}) => (
+<ul className="friend-list">
+    {friends.map((friend,index) => (
+      <li className="item" key={index}>
+      <span className="status">{friend.isOnline}</span>
+      <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+      <p className="name">{friend.name}</p>
+    </li>
+    ))}
+  </ul>
+)
---
+++ my-app/src/statistics.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../src/index.css";
+export const Statistics = ({stats}) => (
+    <section className="statistics">
+    <h2 className="title">Upload stats</h2>
+    <ul className="stat-list">
+  {stats.map((info,index) => (
+      <li className="item" key={index}>
+      <span className="label">{info.label}</span>
+      <span className="percentage">{info.percentage}</span>
+    </li>
+  ))}
+ </ul>
+    </section>
+
+
+)
